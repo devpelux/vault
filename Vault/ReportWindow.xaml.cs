c7cf7a86da3ae0f46134baa5240ff683113db1fd
devpelux <info@devpelux.xyz>
2022-05-16 13:31:21 +0200

    Added ReportWindow.

---
+++ Vault/ReportWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using FullControls.SystemComponents;
+using System.Windows;
+
+namespace Vault
+{
+    /// <summary>
+    /// Window for managing reports.
+    /// </summary>
+    public partial class ReportWindow : AvalonWindow
+    {
+        /// <summary>
+        /// Initializes a new <see cref="ReportWindow"/>.
+        /// </summary>
+        public ReportWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Executed when the window is loaded.
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Executed when the new report button is clicked.
+        /// </summary>
+        private void NewReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
